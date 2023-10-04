4fb1d3ec43f3e523e6842daedbb915b4404a1745
Milos <milos3design@hotmail.com>
2023-10-04 08:47:01 +0200

    json questions implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+
+const initialState = {
+  questions: [],
+  status: "loading", // loading, error, ready, active, finished
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    default:
+      throw new Error("Something's wrong");
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => console.error("Error"));
+  }, []);
+
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        <p>15</p>
+      </Main>
     </div>
   );
 }
